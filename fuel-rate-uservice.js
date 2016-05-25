1c906fc2616f9a1c91fff438dcd44f9b51c9de26
Yang Wang <wang701@purdue.edu>
2016-05-25 14:38:49 -0400

    Improve: make uservice both producer and consumser

--- fuel-rate-uservice.js
+++ fuel-rate-uservice.js
@@ -7,56 +7,74 @@ let type = require('./avro-types');
 let pgns = require('./pgns');
 
 let raw_isobus_type = type.raw_isobus_type;
+let fuel_rate_type = type.fuel_rate_type;
 
-// Create Kafka consumer
-let HighLevelConsumer = kafka.HighLevelConsumer;
-let client = new kafka.Client('vip1.ecn.purdue.edu:2181/');
-let consumer = new HighLevelConsumer(
-		client,
+// Creat Kafka consumer
+let Consumer = kafka.Consumer;
+let cons_client = new kafka.Client('vip1.ecn.purdue.edu:2181');
+let consumer = new Consumer(
+		cons_client,
 		[
-			{ topic: 'raw-isobus' }
+			{ topic: 'raw-isobus-888' }
 		],
 		{
-			encoding: 'buffer'
+			encoding: 'buffer',
+			autoCommit: true
 		}
 );
 
-// listen on topic
-consumer.on('message', function(message) {
-	let msg_buffer = raw_isobus_type.fromBuffer(message.value);
-	let p = pgns[msg_buffer.pgn];
-	if (p && msg_buffer.pgn === 65266) {
-		let data = p.parse(new Buffer(msg_buffer.data, 'hex'));
+// Create Kafka producer
+let Producer = kafka.Producer;
+let prod_client = new kafka.Client('vip1.ecn.purdue.edu:2181');
+let producer = Promise.promisifyAll(new Producer(prod_client));
 
-		console.log('ts:', msg_buffer.timestamp, 'fuel rate:', data.fuel_rate_lhr);
-	}
-});
+// Create work promise/queue
+let work = Promise.resolve();
 
-/*
+// Listen on topic
 producer.on('ready', function() {
-	console.log('after ready');
-	let payloads = [{
-		topic: 'gps-latlon',
-		messages: buf
-	}];
+	consumer.on('message', function(message) {
+		// process the message
+		let rx_buf = raw_isobus_type.fromBuffer(message.value);
+		let p = pgns[rx_buf.pgn];
+		let data;
+		let payloads;
+		if (p && rx_buf.pgn === 65266) {
+			data = p.parse(new Buffer(rx_buf.data, 'hex'));
 
-	producer.sendAsync(payloads)
-	.then(function() {
-		prod_client.close();
-	})
-	.catch(function(err) {
-		console.error(err);
-		process.exit();
-	})
+			let tx_buf = fuel_rate_type.toBuffer({
+					timestamp: rx_buf.timestamp,
+					fuelrate: data.fuel_rate_lhr
+			});
+
+			payloads = [{
+					topic: 'fuel-rate',
+					messages: tx_buf
+			}];
+
+			console.log('ts:', rx_buf.timestamp, 'fuel rate:', data.fuel_rate_lhr);
+		}
+
+		if (payloads) {
+			work = work.then(() => {
+				return producer.sendAsync(payloads);
+			})
+			.catch(function(err) {
+				console.error(err);
+				process.exit();
+			});
+		}
+	});
 });
 
 producer.on('error', function(err) {
 	console.error(err);
 	process.exit();
 });
-*/
 
 process.on('exit', function() {
 	cons_client.close();
+	prod_client.close();
+	consumer.commit();
 	consumer.close();
 });
